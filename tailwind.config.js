2387900f724341958cccb0e0e5823aaa197f015c
RanaPranav22 <pranavrana019@gmail.com>
2024-10-28 12:07:44 +0530

    - Created the react app using "npx create-react-app youtube-clone".
    - Configured Tailwind CSS to the application.

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  // run all those files that are in src folder with the extension [ .js || .jsx ||  .ts || .tsx ]
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
